2173b9236f1736f7f3b662e3b7851d7543c1ed7b
Sunny Vinay <sunnyvinay7@gmail.com>
2024-08-11 15:03:32 -0700

    db + api connection works

--- db.config.js
+++ db.config.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres", // or "me"
+    user: "me",
     host: "localhost",
     database: "pinpal",
     password: "password",
--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const port = 3000;
 
 // MIDDLEWARE
 app.use(cors());
-app.use(express.json); // for access to client JSON requests
+app.use(express.json()); // for access to client JSON requests
 
 // ROUTES
 // Create a user
 app.post('/users', async (req, res) => {
     try {
-        const { username, pass, profile_pic, email, phone_no, loc } = req.body;
+        const { username, pass, profile_pic,email, phone_no, loc } = req.body;
         const newUser = await pool.query(
             "INSERT INTO users (username, pass, profile_pic, email, phone_no, loc) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *", 
             [username, pass, profile_pic, email, phone_no, loc]
